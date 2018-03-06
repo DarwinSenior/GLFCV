1e725fc038ffccba899e2efd1d3fd02cf98d5e82
dingcheng <darwinsenior@gmail.com>
2018-03-06 15:11:02 +0000

    first commit

---
+++ pythonbinding/src/python_module.cpp
@@ -0,0 +1,105 @@
+#define PY_ARRAY_UNIQUE_SYMBOL pbcvt_ARRAY_API
+
+#include <boost/python.hpp>
+#include <boost/foreach.hpp>
+#include <pyboostcvconverter/pyboostcvconverter.hpp>
+#include <iostream>
+#include "../../src/lf-depth-est.h"
+#include "../../src/decoder.h"
+#include "../../src/helper.h"
+
+namespace GLFCV {
+
+    using namespace boost::python;
+
+    std::vector<std::vector<cv::Mat> > lf2mats(PyObject* lf) {
+      std::vector<std::vector<cv::Mat> > mats(0);
+      PyObject* it1 = PyObject_GetIter(lf);
+      PyObject* item1;
+      int i = 0;
+      while (item1 = PyIter_Next(it1)) {
+        PyObject* it2 = PyObject_GetIter(item1);
+        PyObject* item2;
+        std::vector<cv::Mat> mat2(0);
+        while (item2 = PyIter_Next(it2)) {
+          mat2.push_back(pbcvt::fromNDArrayToMat(item2));
+          Py_DECREF(item2);
+        }
+        mats.push_back(mat2);
+        Py_DECREF(it2);
+        Py_DECREF(item1);
+      }
+      Py_DECREF(it1);
+      return mats;
+    }
+
+    PyObject* disparity_map(
+        PyObject* lf,
+        float disp_min, float disp_max) {
+      cv::Mat Dmat;
+      auto lfmat = lf2mats(lf);
+      BuildLFDisparityMap(lfmat, Dmat, disp_min, disp_max);
+      auto ret = pbcvt::fromMatToNDArray(Dmat);
+      return ret;
+    }
+
+    PyObject* getLF(std::string repo) {
+      using namespace std;
+      boost::filesystem::path path(repo);
+      vector<vector<cv::Mat> > lf;
+      cv::Mat disparityGT;
+      struct image_meta metadata;
+      metadata.isValid = false;
+      decoder::ReadBenchmarkFolder(path, lf, disparityGT, &metadata);
+      auto ret = pbcvt::fromMatToNDArray(disparityGT);
+      return ret;
+    }
+
+//This example uses Mat directly, but we won't need to worry about the conversion
+/**
+ * Example function. Basic inner matrix product using implicit matrix conversion.
+ * @param leftMat left-hand matrix operand
+ * @param rightMat right-hand matrix operand
+ * @return an NdArray representing the dot-product of the left and right operands
+ */
+    cv::Mat dot2(cv::Mat leftMat, cv::Mat rightMat) {
+        auto c1 = leftMat.cols, r2 = rightMat.rows;
+        if (c1 != r2) {
+            PyErr_SetString(PyExc_TypeError,
+                            "Incompatible sizes for matrix multiplication.");
+            throw_error_already_set();
+        }
+        cv::Mat result = leftMat * rightMat;
+
+        return result;
+    }
+
+
+#if (PY_VERSION_HEX >= 0x03000000)
+
+    static void *init_ar() {
+#else
+        static void init_ar(){
+#endif
+        Py_Initialize();
+
+        import_array();
+        return NUMPY_IMPORT_ARRAY_RETVAL;
+    }
+
+    BOOST_PYTHON_MODULE (GLFCV) {
+        //using namespace XM;
+        init_ar();
+
+        //initialize converters
+        to_python_converter<cv::Mat,
+                pbcvt::matToNDArrayBoostConverter>();
+        pbcvt::matFromNDArrayBoostConverter();
+
+        //expose module-level functions
+        def("disparity_map", disparity_map);
+        def("getLF", getLF);
+
+    }
+
+} //end namespace pbcvt
